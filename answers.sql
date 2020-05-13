c2a5fdfc90766c49e910f82a294434d39cbb90a5
muhammeta7 <muhammeta7@gmail.com>
2020-05-12 22:22:10 -0400

    finished up to question 5

--- answers.sql
+++ answers.sql
@@ -55,3 +55,18 @@ ON
 (id=matchid)
 WHERE go.teamid
 ='GER';
+
+-- Show the team1, team2 and player for every goal scored by a player called Mario player LIKE 'Mario%'
+SELECT ga.team1, ga.team2,   go.player
+FROM goal go
+JOIN  game ga
+on go.matchid
+= ga.id
+WHERE go.player
+LIKE 'Mario%';
+
+-- Show player, teamid, coach, gtime for all goals scored in the first 10 minutes gtime<=10
+SELECT player, teamid, coach, gtime
+FROM goal
+    JOIN eteam ON teamid=id
+WHERE gtime<=10;
