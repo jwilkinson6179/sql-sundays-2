cc66b261beae0d49d05b6427e01b2a395ae0eef1
muhammeta7 <muhammeta7@gmail.com>
2020-05-19 15:36:44 -0400

    completed wheather station questions

--- tables/hackerrank.sql
+++ tables/hackerrank.sql
@@ -69,7 +69,19 @@ FROM STATION;
 SELECT DISTINCT CITY
 FROM STATION
 WHERE CITY
-REGEXP "^[aeiou].*";
+REGEXP "^[AEIOU]";
+
+-- Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY
+REGEXP "^[^AEIOU]";
+
+-- Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY
+REGEXP "[aeiou]$";
 
 -- Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
 SELECT DISTINCT CITY
@@ -81,6 +93,27 @@ SELECT DISTINCT CITY
 FROM STATION
 WHERE CITY
 NOT REGEXP "[aeiou]$";
+
+-- Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as
+-- both their first and last characters. Your result cannot contain duplicates.
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY
+REGEXP "^[AEIOU].*[aeiou]$";
+
+-- Query the list of CITY names from STATION that either do not start with vowels 
+-- or do not end with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY
+FROM STATION
+WHERE 
+(CITY
+REGEXP "^[^AEIOU]" OR CITY REGEXP"[^aeiou]$");
+
+-- Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY
+REGEXP "^[^AEIOU].*[^aeiou]$";
 
 -- https://www.hackerrank.com/challenges/the-report/problem
 SELECT (CASE WHEN GRADE <8 THEN NULL ELSE name END) name, GRADE, MARKS
