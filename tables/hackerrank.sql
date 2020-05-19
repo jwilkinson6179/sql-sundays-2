3618e806f0626d910a30bc45680112dd221a5a0d
muhammeta7 <muhammeta7@gmail.com>
2020-05-19 14:58:16 -0400

    completed more queries up to regexp

--- tables/hackerrank.sql
+++ tables/hackerrank.sql
@@ -43,3 +43,22 @@ FROM STATION;
 
 -- Query a list of CITY names from STATION with even ID numbers only. 
 -- You may print the results in any order, but must exclude duplicates from your answer.
+SELECT DISTINCT CITY
+FROM STATION
+WHERE MOD(ID, 2) = 0;
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE ID%2 = 0;
+
+-- Let  be the number of CITY entries in STATION, and let  be the number of distinct CITY names in STATION; 
+-- query the value of  from STATION. In other words, find the difference between 
+-- the total number of CITY entries in the table and the number of distinct CITY entries in the table.
+SELECT COUNT(CITY) - COUNT(DISTINCT CITY)
+FROM STATION;
+
+-- Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY
+REGEXP "^[aeiou].*";
