925b9831bfb70be04641662175890d45552618fe
muhammeta7 <muhammeta7@gmail.com>
2020-05-20 01:38:07 -0400

    aggregate functions

--- tables/hackerrank.sql
+++ tables/hackerrank.sql
@@ -33,6 +33,34 @@ SELECT NAME
 FROM CITY
 WHERE COUNTRYCODE='JPN';
 
+-- Query a count of the number of cities in CITY having a Population larger than 100,00.
+SELECT COUNT(NAME)
+FROM CITY
+WHERE POPULATION > 100000;
+
+-- Query the total population of all cities in CITY where District is California.
+SELECT SUM(POPULATION)
+FROM CITY
+WHERE DISTRICT = 'California';
+
+-- Query the average population of all cities in CITY where District is California.
+SELECT AVG(POPULATION)
+FROM CITY
+WHERE DISTRICT = 'California';
+
+-- Query the average population for all cities in CITY, rounded down to the nearest integer.
+SELECT FLOOR(AVG(POPULATION))
+FROM CITY;
+
+-- Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCODE for Japan is JPN.
+SELECT SUM(POPULATION)
+FROM CITY
+WHERE COUNTRYCODE='JPN';
+
+-- Query the difference between the maximum and minimum populations in CITY.
+SELECT MAX(POPULATION)-MIN(POPULATION)
+FROM CITY;
+
 -- Given table STATION with following columns
 -- (ID, CITY, STATE, LAT_N, LONG_N)(NUMBER, VARCHAR, VARCHAR, NUMBER, NUMBER)
 -- =============================================================================================================================
